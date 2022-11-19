3fb73b2362d75f896204d085ca3ad263f6b58368
IgorPietrzak <igor.pietrzak70@gmail.com>
2022-11-19 15:52:16 +0000

    Generate cayley table.

--- groups/Zn.py
+++ groups/Zn.py
@@ -1,11 +1,12 @@
 from math import gcd
-
+from sympy import *
 # This is a multiplicative group of integers modulo n
 
 class Zn:
     def __init__(self, number):
         self.number = number
         self.order = len(self.make())
+        self.is_Abelian = True
 
     def make(self):
         zn = []
@@ -28,4 +29,17 @@ class Zn:
         for i in self.make():
             print(f"Order({i}): " + str(self.e_order(i)))
             print("--------------")
+    
+    def op(self, a, b):
+        return (a*b) & self.number
+
+    def cayley(self):
+        row = self.make()
+        row.insert(0,0)
+        print(row)
+        for i in self.make():
+            list = [self.op(i,j) for j in self.make()]
+            list.insert(0,i)
+            print(list)       
+                
     
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 from groups.Zn import Zn
+from tabulate import tabulate
 
+
+def main():
+
+    z16 = Zn(16)
+    z15 = Zn(15)
+
+    print(f"order of Z16: {z16.order}")
+    print(f"order of Z15: {z15.order}")
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
